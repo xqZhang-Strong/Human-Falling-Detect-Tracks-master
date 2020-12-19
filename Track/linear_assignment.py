49300d0a71121ccf0b4901776c5329737a30e1f8
xqZhang-Strong <32288752+xqZhang-Strong@users.noreply.github.com>
2020-12-19 20:10:37 +0800

    Update linear_assignment.py

--- Track/linear_assignment.py
+++ Track/linear_assignment.py
@@ -64,7 +64,10 @@ def min_cost_matching(distance_metric, max_distance, tracks, detections,
     cost_matrix = distance_metric(tracks, detections, track_indices, detection_indices)
     cost_matrix[cost_matrix > max_distance] = max_distance + 1e-5
     indices = linear_assignment(cost_matrix)
+    print(indices)
     indices = np.array(indices)
+    print('==========')
+    print(indices)
 
     matches, unmatched_tracks, unmatched_detections = [], [], []
     for col, detection_idx in enumerate(detection_indices):
